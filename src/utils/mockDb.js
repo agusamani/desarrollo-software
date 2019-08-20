2c8efefcca631fe858f869ca9913cced7f2d0402
Juan Agustin Amani <juan.amani@globant.com>
2019-08-20 16:37:49 -0300

    hi2

--- src/utils/mockDb.js
+++ src/utils/mockDb.js
@@ -1,35 +1,10 @@
+const userDb = [
+    {},
+    {}
+]
 
-export default class fakeDb{
-    constructor() {
-        this.users = [
-            {
-                id:1,
-                name: 'Mark Teer Stegen',
-                category: 'Plomero',
-            },
-            {
-                id:2,
-                name: 'Nelson Semedo'},
-            {
-                id:3,
-                name: 'Gerrard Pique'},]
-    };
+const categoriesDb = [
 
-    getAll(){
-        return this.users;
-    }
-    
-    getById(id){
-        return this.users.filter(user => user.id === id);
-    }
+]
 
-    getByCategory(category){
-        const item = this.users.filter(user => user.category === category); 
-        return item || null;
-    }
-
-    storeUser(user){
-        if(user) this.users.push(user);
-    }
-
-}
+const title 

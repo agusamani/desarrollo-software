f042c1471597f32b3a2ec782d978ca5ac6c398bc
Juan Agustin Amani <juan.amani@globant.com>
2019-08-22 10:39:53 -0300

    last

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -12,6 +12,15 @@ class Sidebar extends React.Component {
            <Link to="/filter/gasista">Gasista</Link>
            <Link to="/filter/electricista">Electricista</Link>
            <Link to="/filter/albañil">Albañil</Link>
+           <Link to="/filter/conductor">Conductor</Link>
+           <Link to="/filter/chef">Chef</Link>
+           <Link to="/filter/diseñador">Diseñador</Link>
+           <Link to="/filter/fotografo">Fotografo</Link>
+           <Link to="/filter/mecanico">Mecanico</Link>
+           <Link to="/filter/periodista">Periodista</Link>
+           <Link to="/filter/policia">Policia</Link>
+           <Link to="/filter/sercretario">Sercretario</Link>
+           <Link to="/filter/traductor">Traductor</Link>
           </div>
         )
       }
